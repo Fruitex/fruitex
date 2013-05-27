7302f605694d15e10fa07d0309a83b042417015c
Shin Fan <xinyuan.f@gmail.com>
2013-05-27 19:21:59 -0400

    Add git ignore file and enable user admin site

--- fruitex/settings.py
+++ fruitex/settings.py
@@ -1,4 +1,5 @@
 # Django settings for fruitex project.
+from config import Config
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +12,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': Config.DatabaseName,
+        # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -121,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
