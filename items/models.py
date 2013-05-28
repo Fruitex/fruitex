6a81d38bed70d7883f74b2e76bd34e3373449292
Shin Fan <xinyuan.f@gmail.com>
2013-05-28 12:49:50 -0400

    Add __unicode__ to functions in models.py

--- items/models.py
+++ items/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Store(models.Model):
+    def __unicode__(self):
+        return self.name
     name = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
 
 class Item(models.Model):
+    def __unicode__(self):
+        return self.name
     store = models.ForeignKey(Store)
     category = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
