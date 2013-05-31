9a8dc46ae4732bc947c7433dda79562151c2cc86
Bi Ran <biran0079@gmail.com>
2013-05-31 00:34:48 -0700

    Improve viewlist

--- items/views.py
+++ items/views.py
@@ -4,5 +4,5 @@ from items.models import Store, Item
 
 def itemlist(request):
     template = loader.get_template('itemlist.html')
-    context = Context({'itemList' : Item.objects.all()})
+    context = Context({'itemList' : Item.objects.all()[:4]})
     return HttpResponse(template.render(context))
