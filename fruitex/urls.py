2dba7d1d1e6bcf22b11fc01e93e03a30c40db810
Shin Fan <xinyuan.f@gmail.com>
2013-06-28 03:10:11 -0400

    Add intro page

--- fruitex/urls.py
+++ fruitex/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'fruitex.views.home', name='home'),
     # url(r'^fruitex/', include('fruitex.foo.urls')),
                        
-    url(r'^home/', 'fruitex.views.home', name='home'),
+    url(r'^$', 'fruitex.views.home', name='home'),
     url(r'^items/', include('items.urls')),
     url(r'^cart/', 'cart.views.cart'),
 
