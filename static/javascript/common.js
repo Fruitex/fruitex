68efbee1489bbc564670a57614e186b3fb4137ec
Bi Ran <biran0079@gmail.com>
2013-09-01 17:56:16 -0400

    Add js code to parse query.

--- static/javascript/common.js
+++ static/javascript/common.js
@@ -3,3 +3,28 @@ function getURLParameter(name) {
         (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,''])[1]
     );
 }
+var RegExpFindAll = function (s, p, idx) {
+  var r = new RegExp(p);
+  var res = [];
+  var match;
+  while(match = r.exec(s)) {
+    res.push(match[idx]);
+  }
+  return res;
+};
+var ParseQuery = function (query) {
+  var storeP = /store:([^ ]*)/g;
+  var stores = RegExpFindAll(query, storeP, 1);
+  query = query.replace(storeP, '');
+  var cateP = /cate:([^' ]+)/g;
+  var cates = RegExpFindAll(query, cateP, 1);
+  query = query.replace(cateP, '');
+  cateP = /cate:'([^']+)'/g;
+  cates.push.apply(cates, RegExpFindAll(query, cateP, 1));
+  query = query.replace(cateP, '');
+  return {
+    store: stores,
+    cate: cates,
+    keyword: query.replace(/^\s+|\s+$/, ''),
+  };
+};
--- static/javascript/store_list.js
+++ static/javascript/store_list.js
@@ -2,22 +2,29 @@
 var StoreList = function () {
 	var init = function () {
 		$('#store-list').children('li:not(:first-child)').click(function () {
-			window.location = '/home/?store=' + $(this).attr('id');
+			window.location = '/home/?query=' + 'store:' + $(this).attr('id');
 		})
-		var curStore = getURLParameter('store') ? getURLParameter('store') : 'sobeys';
-		if ($('#store-list').children('#' + curStore)) {
-			$('#store-list').children('#' + curStore).append(
-				$('<img>')
-					.addClass('store-list-green-dot')
-					.attr('src', '{% static "imgs/green_dot.png" %}')
-			);
-		};
+		var curStore = ParseQuery(getURLParameter('query')).store[0];
+    if (!curStore) {
+      curStore = 'sobeys';
+    }
+    SelectCurrentStoreUI(curStore);
 	};
 
 	return {
 		init: init
 	};
 }();
+
+var SelectCurrentStoreUI = function (curStore) {
+  if ($('#store-list').children('#' + curStore)) {
+    $('#store-list').children('#' + curStore).append(
+      $('<img>')
+        .addClass('store-list-green-dot')
+        .attr('src', '{% static "imgs/green_dot.png" %}')
+    );
+  };
+}
 
 $(document).ready(function () {
 	StoreList.init();
