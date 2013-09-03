73771d2485037676d2b8a0840edc20b07cfde8ec
Bi Ran <biran0079@gmail.com>
2013-09-03 14:49:18 -0400

    Delete unused folder.

--- orders/urls.py
+++
@@ -1,4 +0,0 @@
-from django.conf.urls import patterns, url
-from orders import views
-
-urlpatterns = patterns('', url(r'^$', views.orderlist))
--- orders/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import Context, loader
-from home.models import Store, Item
-from django.shortcuts import redirect
-from fruitex.views import error
-
