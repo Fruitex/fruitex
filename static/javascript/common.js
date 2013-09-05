6827df22b5b55cb7d1969ec4de0aa7eda9dad1ee
Bi Ran <biran0079@gmail.com>
2013-09-05 02:24:43 -0400

    Add google analytics tracking code.

--- static/javascript/common.js
+++ static/javascript/common.js
@@ -35,3 +35,15 @@ var GetSelectedStore = function() {
   }
   return curStore;
 }
+
+/*
+ *  Google analytics code, don't touch.
+ */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43781659-1', 'fruitex.ca');
+ga('send', 'pageview');
+
