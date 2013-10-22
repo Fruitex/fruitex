0cddc78354f6e364fc29500b0011d5ac23c498d7
liuxiaogang <not-found@qq.com>
2013-10-22 14:07:17 +0800

    new check order

--- home/tests.py
+++ home/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.core.mail import send_mail
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,5 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+def s_email():
+    send_mail('no reply','', 'noreply@fruitex.com',['not-found@qq.com','luregun@gmail.com'], fail_silently=False,html_message='<html><body>Test</body></html>')
