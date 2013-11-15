43a5d27ae2cbcf1f620385103978c8d244592b72
Greg Wang <gregwym@gmail.com>
2013-11-14 19:21:19 -0500

    Enhance admin config for home and cart models.

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from home.models import Item
 from home.models import Store
 
-admin.site.register(Store)
-admin.site.register(Item)
+class StoreAdmin(admin.ModelAdmin):
+  list_display = ["name", "id", "address"]
+  ordering = [ "-id" ]
+  search_fields = ["id", "name"]
+
+admin.site.register(Store, StoreAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+  list_display = [
+    "name", "id", "sku", "price", "sales_price", "store", "category",
+    "sold_number", "out_of_stock"
+  ]
+  list_filter = [ "store", "out_of_stock", "tax_status", "tax_class" ]
+  ordering = [ "-id" ]
+  search_fields = ["id", "name", "sku"]
+
+admin.site.register(Item, ItemAdmin)
