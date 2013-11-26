4f076e9f11d52ac0c298b7091d4e86aec446090c
Greg Wang <gregwym@gmail.com>
2013-11-26 17:32:33 -0500

    Default value for DecimalField should be in type Decimal.

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 class Store(models.Model):
     def __unicode__(self):
@@ -24,8 +25,8 @@ class Item(models.Model):
 
     # Tax Classes
     TAX_CLASSES = (
-        (0.0, 'Non-Taxable'),
-        (0.13, 'Standard-Rate'),
+        (Decimal('0.0'), 'Non-Taxable'),
+        (Decimal('0.13'), 'Standard-Rate'),
     )
 
     name = models.CharField(max_length=200)
@@ -35,7 +36,7 @@ class Item(models.Model):
     sales_price = models.DecimalField(max_digits=16, decimal_places=2, default=0)
     out_of_stock = models.BooleanField(default=False)
     on_sale = models.BooleanField(default=False)
-    tax_class = models.DecimalField(max_digits=3, decimal_places=2, choices=TAX_CLASSES, default=0.0)
+    tax_class = models.DecimalField(max_digits=3, decimal_places=2, choices=TAX_CLASSES, default=Decimal('0.0'))
     sold_number = models.IntegerField(editable=False, default=0)
     when_added = models.DateTimeField(auto_now_add=True)
     when_updated = models.DateTimeField(auto_now=True)
