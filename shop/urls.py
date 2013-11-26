67d5dcad591aed15c5414d4bb3cf71f8f9f651c7
Greg Wang <gregwym@gmail.com>
2013-11-26 17:27:27 -0500

    Use category prefix for all category.

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('shop.views',
   url(r'^$', 'to_default', name='to_default'),
   url(r'^(?P<store_slug>\w+)/$', 'store_front', name='store_home'),
-  url(r'^(?P<store_slug>\w+)/(?P<category_id>\d+)/$', 'store_front', name='store_category'),
+  url(r'^(?P<store_slug>\w+)/category/(?P<category_id>\d+)/$', 'store_front', name='store_category'),
 )
