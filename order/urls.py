92b2b46b78e66c076aaff841c7e4d5fb9a624043
Greg Wang <gregwym@gmail.com>
2013-11-28 01:59:00 -0500

    Implement cart view with serverside template.

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('order.views',
   # Views
+  url(r'^cart/$', 'view_cart', name='cart'),
   url(r'^new/$', 'new_order', name='new'),
 )
--- order/views.py
+++ order/views.py
@@ -1,16 +1,43 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
+from django.core import serializers
 
 from shop.models import Item
 
 import urllib
 import json
 
+# Common operations
+
+def cart_from_request(request):
+  cart = urllib.unquote(request.COOKIES.get('cart'))
+  cart = json.loads(cart)
+  return cart
+
+def cart_items(cart):
+  items = Item.objects.filter(id__in=cart)
+  json_value = serializers.serialize('json', items)
+  json_value = json.loads(json_value)
+  return [{
+    'obj': item,
+    'quantity': cart.count(item.id),
+    'json': json.dumps(json_value[i])
+  } for i, item in enumerate(items)]
+
 # Views
 
 @csrf_exempt
+def view_cart(request):
+  template = loader.get_template('order/cart.html')
+
+  cart = cart_from_request(request)
+  items = cart_items(cart)
+
+  context = Context({
+    'items': items,
+  })
+  return HttpResponse(template.render(context))
+
 def new_order(request):
-  cart = urllib.unquote(request.COOKIES.get('cart'))
-  cart = json.loads(cart)
-  return HttpResponse(json.dumps(cart))
+  return HttpResponse('not implemented')
