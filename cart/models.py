29d42be71bb3846cb542ce7f166ddf6c8f06eec9
Greg Wang <gregwym@gmail.com>
2013-12-04 23:08:12 -0500

    Clean up cart app, leave only models and admin.

--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,4 @@
-from django.core.mail import send_mail
 from django.db import models
-from django.db.models import F
-from home.models import Item
-import json
-from django.core.mail import EmailMessage
-from django.template import loader
-from config.email import EMAIL_HOST_USER
 
 # Create your models here.
 class Order(models.Model):
@@ -33,41 +26,3 @@ class Coupon(models.Model):
   code = models.CharField(max_length=30)
   value = models.FloatField()
   used = models.BooleanField()
-
-
-from paypal.standard.ipn.signals import payment_was_successful
-from paypal.standard.ipn.signals import payment_was_flagged
-from threading import Thread
-
-def send_receipt(to, invoice):
-  Thread(target=lambda:send_html_message(to,invoice)).start()
-
-def handle_payment_received(status, ipn):
-  invoice = ipn.invoice
-  coupon = json.loads(ipn.custom)['coupon']
-  #invalidate coupon
-  Coupon.objects.filter(code=coupon).update(used=True)
-  order = Order.objects.filter(invoice=invoice)[0]
-  order.status=status
-  order.email=ipn.payer_email
-  order.save()
-  ids = json.loads(order.items)
-  for it in Item.objects.filter(id__in=set(ids)):
-    it.sold_number=it.sold_number + ids.count(it.id)
-    it.save()
-  send_receipt(ipn.payer_email,invoice)
-
-def payment_successful(sender, **kwargs):
-  handle_payment_received('paid', sender)
-
-def payment_flagged(sender, **kwargs):
-  handle_payment_received('flagged', sender)
-
-payment_was_successful.connect(payment_successful)
-payment_was_flagged.connect(payment_flagged)
-
-def send_html_message(to,invoice):
-    html_content = loader.render_to_string('reply.html',{'invoice':invoice})
-    msg = EmailMessage('[Fruitex] Payment of your order %s received.' % invoice,html_content,EMAIL_HOST_USER,[to])
-    msg.content_subtype = "html"
-    msg.send()
