a3dfcc1d88bff03240ece2a8ea3622a9e6c7c7b1
Greg Wang <gregwym@gmail.com>
2013-12-09 18:29:53 -0500

    Add CheckoutForm for easier server-side form validation.

--- order/views.py
+++ order/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.template import Context, loader, RequestContext
 from django.core import serializers
 from django.core.urlresolvers import reverse
+from django.core.validators import RegexValidator
 from django.conf import settings
+from django import forms
+from django.core.exceptions import ValidationError
 
 from paypal.standard.forms import PayPalPaymentsForm
 from querystring_parser import parser
@@ -16,6 +19,27 @@ import uuid
 
 from shop.models import Item, DeliveryOption
 from order.models import Order, OrderItem, Invoice, Coupon, DeliveryWindow
+
+# Checkout form definition
+
+class CheckoutForm(forms.Form):
+  def _validate_coupon_code(coupon_code):
+    coupon = Coupon.objects.get_valid_coupon(coupon_code)
+    if coupon == False:
+      raise ValidationError(u'%s is not a valid coupon' % coupon_code)
+
+  name = forms.CharField(max_length=64, validators=[
+    RegexValidator(regex=r'^[a-zA-Z ]+$'),
+  ])
+  phone = forms.CharField(max_length=16, validators=[
+    RegexValidator(regex=r'^(\+1){0,1}\({0,1}\d{3}[ -\)]{0,1}\d{3}[ -]{0,1}\d{4}$'),
+  ])
+  email = forms.EmailField()
+  address = forms.CharField(min_length=8)
+  postcode = forms.CharField(max_length=8, validators=[
+    RegexValidator(regex=r'^[a-zA-Z]\d[a-zA-Z] {0,1}\d[a-zA-Z]\d$'),
+  ])
+  coupon_code = forms.CharField(required=False, validators=[_validate_coupon_code])
 
 # Common operations
 
