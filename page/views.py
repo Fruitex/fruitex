fe221a6c9be6f891c494c17f1cba4d1c19f4f9eb
Greg Wang <gregwym@gmail.com>
2014-01-07 00:58:29 -0500

    Limit homepage number of store logos to 5.

--- page/views.py
+++ page/views.py
@@ -1,13 +1,10 @@
 from django.http import HttpResponse
 from django.template import Context, loader
-from django.utils.timezone import make_aware, get_default_timezone
-
-from datetime import datetime, timedelta
 
 from shop.models import Store
 
 def home(request):
-  stores = Store.objects.all()
+  stores = Store.objects.order_by('id')[:5]
 
   context = Context({
     'stores': stores,
