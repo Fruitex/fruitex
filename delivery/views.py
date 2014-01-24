cf7ed8075d87527e765a27a114612b74251e8444
Greg Wang <gregwym@gmail.com>
2014-01-23 23:19:40 -0500

    Divide and display delivery windows by days.

--- delivery/views.py
+++ delivery/views.py
@@ -1,17 +1,29 @@
 from django.http import HttpResponse
 from django.template import Context, loader
-from django.utils.timezone import make_aware, get_default_timezone
+from django.utils.timezone import make_aware, get_default_timezone, localtime
 
 from datetime import datetime, timedelta
 
 from order.models import DeliveryWindow, Order
 
 def summary(request):
+  def divide_delivery_window_by_days(delivery_windows):
+    divided = {}
+    for delivery_window in delivery_windows:
+      date = localtime(delivery_window.start).date()
+      if date in divided:
+        divided[date].append(delivery_window)
+      else:
+        divided[date] = [delivery_window]
+    return divided
+
   datetime_threshold = make_aware(datetime.now() - timedelta(days=30), get_default_timezone())
   delivery_windows = DeliveryWindow.objects.filter(start__gt=datetime_threshold).order_by('-start', 'store__id')
+  divided_by_days = divide_delivery_window_by_days(delivery_windows)
 
   context = Context({
     'delivery_windows': delivery_windows,
+    'delivery_windows_divided_by_days': sorted(divided_by_days.items(), reverse=True),
   })
 
   template = loader.get_template('delivery/summary.html')
