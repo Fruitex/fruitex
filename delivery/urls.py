79445bbda474be601b0417008377f652e28bc8e8
Vincent Wen <wensi@live.com>
2014-02-08 03:10:11 -0500

    Added a destinations page, which lists all invoices from delivery windows in the same time frame Re-grouped delivery windows by date and time. Re-styled summaries page to accommodate this change.

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('delivery.views',
   # Views
   url(r'^summary/$', 'summary', name='summary'),
-  url(r'^detail/(?P<id>\d+)/$', 'detail', name='detail')
+  url(r'^detail/(?P<id>\d+)/$', 'detail', name='detail'),
+  url(r'^summary/invoices/(?P<ids>\d+(\+\d+)*)/$', 'invoices', name='invoices')
 
   # API
 )
