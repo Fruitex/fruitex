801ce27f0f8d11f887ef2b69e68233ecdef8e346
Greg Wang <gregwym@gmail.com>
2014-03-06 01:11:12 -0500

    Enhance delivery bucket admin to be able to assign order.

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from delivery.models import DeliveryBucket
+
+from delivery.models import DeliveryBucket, DeliveryBucketOrder
+
+class DeliveryBucketOrderInline(admin.TabularInline):
+  model = DeliveryBucketOrder
+  raw_id_fields = ['delivery_bucket', 'order']
 
 class DeliveryBucketAdmin(admin.ModelAdmin):
   date_hierarchy = 'start'
-  list_display = [
-    'start', 'end', 'driver'
-  ]
+  list_display = ['start', 'end', 'driver']
   ordering = [ '-start' ]
   search_fields = ['start', 'driver']
+  inlines = [DeliveryBucketOrderInline]
 
 admin.site.register(DeliveryBucket, DeliveryBucketAdmin)
