ca3428258b2391f2f9612b7586b3fb5780e12c71
Vincent Wen <wensi@live.com>
2014-03-07 16:28:53 -0500

    Added User Object to REST API

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from shop.models import *
 from order.models import *
+
+# Account
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = User
+    fields = ('id', 'url', 'username', 'first_name', 'last_name', 'email', 'last_login', 'date_joined')
 
 # Shop
 class StoreSerializer(serializers.HyperlinkedModelSerializer):
