26ff0693b197e0ac2d95db013e176d1d4bb95943
Vincent Wen <wensi@live.com>
2014-03-26 21:10:50 -0400

    Changed tabs from 4 spaces to 2 spaces

--- api/views.py
+++ api/views.py
@@ -10,63 +10,63 @@ from delivery.models import *
 
 # Filters
 class ItemFilter(django_filters.FilterSet):
-    min_sold = django_filters.NumberFilter(name="sold_number", lookup_type='gte')
-    max_sold = django_filters.NumberFilter(name="sold_number", lookup_type='lte')
-    class Meta:
-        model = Item
-        fields = ['on_sale', 'out_of_stock', 'featured', 'sold_number', 'min_sold', 'max_sold']
+  min_sold = django_filters.NumberFilter(name="sold_number", lookup_type='gte')
+  max_sold = django_filters.NumberFilter(name="sold_number", lookup_type='lte')
+  class Meta:
+    model = Item
+    fields = ['on_sale', 'out_of_stock', 'featured', 'sold_number', 'min_sold', 'max_sold']
 
 # Account
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    ordering_fields = ['date_joined', 'last_login']
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  ordering_fields = ['date_joined', 'last_login']
 
 # Shop
 class StoreViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Store.objects.all()
-    serializer_class = StoreSerializer
+  queryset = Store.objects.all()
+  serializer_class = StoreSerializer
 
 class StoreCustomizationViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = StoreCustomization.objects.all()
-    serializer_class = StoreCustomizationSerializer
+  queryset = StoreCustomization.objects.all()
+  serializer_class = StoreCustomizationSerializer
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
 
 class ItemViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-    filter_class = ItemFilter
-    ordering_fields = ['sold_number']
+  queryset = Item.objects.all()
+  serializer_class = ItemSerializer
+  filter_class = ItemFilter
+  ordering_fields = ['sold_number']
 
 # Order
 class DeliveryWindowViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = DeliveryWindow.objects.all()
-    serializer_class = DeliveryWindowSerializer
+  queryset = DeliveryWindow.objects.all()
+  serializer_class = DeliveryWindowSerializer
 
 class OrderViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    filter_fields = ['status', 'invoice__invoice_num']
-    ordering = ['-when_created']
-    ordering_fields = ['when_created', 'when_updated', 'subtotal']
+  queryset = Order.objects.all()
+  serializer_class = OrderSerializer
+  filter_fields = ['status', 'invoice__invoice_num']
+  ordering = ['-when_created']
+  ordering_fields = ['when_created', 'when_updated', 'subtotal']
 
 class InvoiceViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Invoice.objects.all()
-    serializer_class = InvoiceSerializer
-    filter_fields = ['status', 'email', 'user__username']
-    ordering = ['-when_created']
-    ordering_fields = ['when_created', 'when_updated', 'subtotal']
+  queryset = Invoice.objects.all()
+  serializer_class = InvoiceSerializer
+  filter_fields = ['status', 'email', 'user__username']
+  ordering = ['-when_created']
+  ordering_fields = ['when_created', 'when_updated', 'subtotal']
 
 class CouponViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Coupon.objects.all()
-    serializer_class = CouponSerializer
+  queryset = Coupon.objects.all()
+  serializer_class = CouponSerializer
 
 # Delivery
 class DeliveryBucketViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = DeliveryBucket.objects.all()
-    serializer_class = DeliveryBucketSerializer
-    filter_fields = ['assignee__username', 'assignee__id']
-    ordering = ['-start']
+  queryset = DeliveryBucket.objects.all()
+  serializer_class = DeliveryBucketSerializer
+  filter_fields = ['assignee__username', 'assignee__id']
+  ordering = ['-start']
