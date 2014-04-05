1ec3c9bdb130f695bfcd9d32a6474adf235316ba
Greg Wang <gregwym@gmail.com>
2014-04-05 19:08:23 -0400

    Refactor account.UserInvoice controller.

--- static/ng/account/controllers.js
+++ static/ng/account/controllers.js
@@ -6,28 +6,17 @@ angular.module('account.controllers', [
   '$scope', '$q', 'InvoiceResource', 'OrderResource',
   function($scope, $q, InvoiceResource, OrderResource) {
     $scope.invoices = [];
-    $scope.invoiceUrlBase = null;
 
     $scope.init = function(userId, username, invoiceUrlBase) {
-      var invoices;
-
       InvoiceResource.query({ user__username: username }).$promise
       .then(function(response) {
-        invoices = response.results;
+        var invoices = $scope.invoices = response.results;
         _.each(invoices, function(invoice) {
           invoice.url = invoiceUrlBase + invoice.id;
-        });
-        var orderPromises = _.flatten(_.map(invoices, function(invoice) {
-          return _.map(invoice.orders, function(orderId, i) {
-            var order = invoice.orders[i] = OrderResource.get({ id: orderId });
-            return order;
+          _.each(invoice.orders, function(orderId, i) {
+            invoice.orders[i] = OrderResource.get({ id: orderId });
           });
-        }));
-
-        return $q.all(orderPromises);
-      })
-      .then(function() {
-        $scope.invoices = invoices;
+        });
       });
     };
   }
