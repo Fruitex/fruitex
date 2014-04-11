199e827583e15d2bdcb5a47e037ac5f34f9f0359
Greg Wang <gregwym@gmail.com>
2014-04-11 00:02:33 -0400

    Fix order:show reverse params.

--- order/views.py
+++ order/views.py
@@ -162,7 +162,7 @@ def checkout(request):
       if payment_method != Payment.METHODS_PAYPAL:
         Payment.objects.create_payment(invoice, payment_method)
         invoice.set_status(Invoice.STATUS_PAY_ON_DELIVERY)
-        response = HttpResponseRedirect(reverse('order:show', kwargs={'invoice_num': invoice.invoice_num}))
+        response = HttpResponseRedirect(reverse('order:show', kwargs={'id': invoice.id}))
         response.set_cookie('cart', '')
       else:
         raw_payment = paypal.create_raw_payment_for_invoice(invoice, {
@@ -219,7 +219,7 @@ def payment_paypal_execute(request, id):
       payment.raw = json.dumps(raw_payment.to_dict())
       payment.set_status(Payment.STATUS_COMPLETED)
       # Clear cookie cart and redirect to show the order.
-      response = HttpResponseRedirect(reverse('order:show', kwargs={'invoice_num': invoice.invoice_num}))
+      response = HttpResponseRedirect(reverse('order:show', kwargs={'id': invoice.id}))
       response.set_cookie('cart', '')
       return response
   return HttpResponse('Failed to execute your payment. Please contact us for help.')
@@ -234,7 +234,7 @@ def payment_paypal_cancel(request, id):
     payments = invoice.payments.all()
     for payment in payments:
       payment.set_status(Payment.STATUS_CANCELLED)
-  return HttpResponseRedirect(reverse('order:show', kwargs={'invoice_num': invoice.invoice_num}))
+  return HttpResponseRedirect(reverse('order:show', kwargs={'id': invoice.id}))
 
 # API
 
