666101aa2f15e80dcba2fe3fedcd388539c41901
Greg Wang <gregwym@gmail.com>
2014-04-12 16:28:24 -0400

    Show order status on account invoices page.

--- static/ng/account/directives.js
+++ static/ng/account/directives.js
@@ -1,8 +1,11 @@
-angular.module('account.directive', [])
-.directive('ngInvoiceRow', function() {
+angular.module('account.directive', ['common.constants'])
+.directive('ngInvoiceRow', function(FruitexConstants) {
   return {
     scope: {
       invoice: '=ngInvoiceRow'
+    },
+    controller: function($scope, $log) {
+      $scope.orderStatus = FruitexConstants.orders.status;
     },
     templateUrl: '/static/ng/account/templates/invoice-row.html'
   };
