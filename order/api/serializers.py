caca46689a2e19eb8f2cce0fa0b4dc2cfbcce6f1
Greg Wang <gregwym@gmail.com>
2014-04-17 01:45:47 -0400

    Remove nested order_items.

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -12,17 +12,19 @@ class DeliveryWindowSerializer(serializers.ModelSerializer):
 
 class InvoiceSerializer(serializers.ModelSerializer):
   user = UserSerializer()
+  orders = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+  total = serializers.Field()
   class Meta:
     model = Invoice
+
+class OrderSerializer(serializers.ModelSerializer):
+  invoice = InvoiceSerializer()
+  total = serializers.Field()
+  delivery_window = DeliveryWindowSerializer()
+  class Meta:
+    model = Order
 
 class OrderItemSerializer(serializers.ModelSerializer):
   item = ItemSerializer()
   class Meta:
     model = OrderItem
-
-class OrderSerializer(serializers.ModelSerializer):
-  order_items = OrderItemSerializer()
-  invoice = InvoiceSerializer()
-  delivery_window = DeliveryWindowSerializer()
-  class Meta:
-    model = Order
