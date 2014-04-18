bf79cf7648217066257693ac4eec9bb4ded1bdbf
Greg Wang <gregwym@gmail.com>
2014-04-18 17:50:29 -0400

    Add current user invoices API

--- account/api/views.py
+++ account/api/views.py
@@ -5,12 +5,16 @@ from rest_framework import permissions
 
 from account.api.serializers import *
 
+from order.models import *
+from order.api.serializers import *
+
 # Views
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
   model = User
   serializer_class = UserSerializer
   ordering_fields = ['date_joined', 'last_login']
 
+# Current user views
 class CurrentUserView(generics.RetrieveAPIView):
   model = User
   serializer_class = UserSerializer
@@ -18,3 +22,10 @@ class CurrentUserView(generics.RetrieveAPIView):
 
   def get_object(self):
     return self.request.user
+
+class CurrentUserInvoicesView(generics.ListAPIView):
+  serializer_class = InvoiceSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_queryset(self):
+    return Invoice.objects.filter(user=self.request.user)
--- fruitex/api/urls.py
+++ fruitex/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 
 from drf_toolbox import routers
-from account.api.views import UserViewSet, CurrentUserView
+from account.api.views import UserViewSet, CurrentUserView, CurrentUserInvoicesView
 from shop.api.views import StoreViewSet, CategoryViewSet, ItemViewSet
 from order.api.views import InvoiceViewSet, OrderViewSet, OrderItemViewSet, DeliveryWindowViewSet
 from delivery.api.views import DeliveryBucketViewSet, DeliveryBucketOrderViewSet
@@ -30,11 +30,12 @@ router.register(r'delivery_buckets/orders', DeliveryBucketOrderViewSet)
 
 # Current user resources
 user_urlpatterns = patterns('',
-  url(r'', CurrentUserView.as_view()),
+  url(r'^$', CurrentUserView.as_view()),
+  url(r'^invoices/$', CurrentUserInvoicesView.as_view()),
 )
 
 urlpatterns = patterns('',
-  url(r'', include(router.urls)),
-  url(r'users/current', include(user_urlpatterns)),
+  url(r'^', include(router.urls)),
+  url(r'^users/current/', include(user_urlpatterns)),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
